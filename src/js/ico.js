0c39c4015e8f01885be59eef5fd221632cf704b2
unknown <danicorreo99@gmail.com>
2019-07-26 23:18:54 +0200

    basics

---
+++ src/js/ico.js
@@ -0,0 +1,64 @@
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.5/d3.min.js"></script>
+//<script src="src/ico.js"></script>
+const dataset = [1, 1, 1, 1, 1, 1, 1, 1, 3, 1]
+
+const colors = ['#9e0142', '#d53e4f', '#f46d43', '#fdae61', '#fee08b', '#e6f598', '#abdda4', '#66c2a5', '#3288bd', '#5e4fa2']
+
+const width = document.querySelector('.app-icon').offsetWidth
+const height = document.querySelector('.app-icon').offsetHeight
+const radius = Math.min(width, height) / 2
+const initialAnimDelay = 300
+const arcAnimDelay = 150
+const arcAnimDur = 3000
+
+// append svg
+let svg = d3.select('.app-icon').append('svg')
+  .attr({
+    'width': width,
+    'height': height,
+    'class': 'pieChart'
+  })
+  .append('g')
+
+svg.attr({
+  'transform': `translate(${width / 2}, ${height / 2})`
+});
+
+// for drawing slices
+let arc = d3.svg.arc()
+  .outerRadius(radius)
+  .innerRadius(radius * 0.9)
+
+
+let pie = d3.layout.pie()
+  .value(d => d)
+pie.padAngle(0.08);
+
+let draw = function() {
+  svg.append("g").attr("class", "slices")
+
+  // define slice
+  let slice = svg.select('.slices')
+    .datum(dataset)
+    .selectAll('path')
+    .data(pie)
+  slice
+    .enter().append('path')
+    .attr({
+      'fill': (d, i) => colors[i],
+      'd': arc,
+      'transform': (d, i) => 'rotate(-180, 0, 0)'
+    })
+    .style('opacity', 0)
+    .transition()
+    .delay((d, i) => (i * arcAnimDelay) + initialAnimDelay)
+    .duration(arcAnimDur)
+    .ease('elastic')
+    .style('opacity', 1)
+    .attr('transform', 'rotate(0,0,0)')
+}
+
+draw()
+
+//d3.selectAll('.slices').transition().ease('back').duration(500).delay(0).style('opacity', 0).attr('transform', 'translate(0, 250)').remove()
+//setTimeout(draw, 800)
---
+++ src/main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow } = require('electron')
+const {autoUpdater} = require("electron-updater");
+
+function createWindow () {
+  // Create the browser window.
+  let win = new BrowserWindow({
+    width: 600,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('src/index.html')
+}
+
+app.on('ready', createWindow)
+
+//AutoUpdater
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
+});
